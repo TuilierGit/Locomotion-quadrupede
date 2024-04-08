3ed56c89fa03d34ccb25868c590b73caa624bceb
TuilierGit <thomas.tuilier.git@gmail.com>
2024-04-08 15:21:55 +0200

    Adding install file

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget https://rhoban.github.io/quadruped/quadruped.zip
+unzip quadruped.zip
+python -m venv profil_robotique
+# source profil_robotique/bin/activate
+# pip3 install numpy scipy pybullet matplotlib
